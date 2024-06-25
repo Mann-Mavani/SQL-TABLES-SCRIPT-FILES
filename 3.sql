6582eca6b369744d41a0bb9fb223ef1a334437bd
Mann-Mavani <129569741+Mann-Mavani@users.noreply.github.com>
2024-06-25 05:36:00 +0530

    Add files via upload

---
+++ 3.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Cricketers (
+    Name VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+INSERT INTO Cricketers (Name, City, Age) VALUES
+('Sachin Tendulkar', 'Mumbai', 30),
+('Rahul Dravid', 'Bombay', 35),
+('M. S. Dhoni', 'Jharkhand', 31),
+('Suresh Raina', 'Gujarat', 30);
+
+
+CREATE TABLE Employees (
+    Name VARCHAR(50),
+    City VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO Employees (Name, City, Age) VALUES
+('Jay Patel', 'Rajkot', 30),
+('Rahul Dave', 'Baroda', 35),
+('Jeet Patel', 'Surat', 31),
+('Vijay Raval', 'Rajkot', 30);
